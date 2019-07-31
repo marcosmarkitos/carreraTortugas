1edaf2115d8e04b1aec9282025762a38b203f674
Marcos Gutiérrez <marcos.gutierrez.hernandez@gmail.com>
2019-07-31 12:05:42 +0200

    creación de circuito con medidas, corredores con colores y método competir

---
+++ main.py
@@ -0,0 +1,43 @@
+import turtle
+import random
+
+class Circuito():
+    corredores = []
+    __posStartY = (-60, -20, 20, 60)
+    __colorTurtle = ('red', 'blue', 'green', 'orange')
+    
+    def __init__(self, width, height):  
+        self.__screen = turtle.Screen()
+        self.__screen.setup(width, height)
+        self.__screen.bgcolor('lightgray')
+        self.__startLine = -width/2 + (5*width/100)
+        self.__finishLine = width/2 - (5*width/100)
+        
+        self.__createRunners()
+        
+    def __createRunners(self):
+        for i in range (4):
+            new_turtle = turtle.Turtle()
+            new_turtle.color(self.__colorTurtle[i])
+            new_turtle.shape('turtle')
+            new_turtle.penup()
+            new_turtle.setpos(self.__startLine, self.__posStartY[i])    
+            
+            self.corredores.append(new_turtle)
+        
+    def competir(self):
+        hayGanador = False
+        while not hayGanador:
+            for tortuga in self.corredores:
+                avance = random.randint(1,6)
+                tortuga.fd(avance)
+                
+                if tortuga.position()[0] >= self.__finishLine:
+                    hayGanador = True
+                    print("La tortuga de color {} ha ganado.".format(tortuga.color()[0]))
+            
+
+
+if __name__ == '__main__':
+    circuito = Circuito(640, 480)
+    circuito.competir()
